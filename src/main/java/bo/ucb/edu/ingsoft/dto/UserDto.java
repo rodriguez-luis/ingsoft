c8582ca55eef6a6719cb919aa74ae5541d3b9a0d
rodriguez-luis <pb.rndm@gmail.com>
2020-12-16 11:48:13 -0400

    add user dto

---
+++ src/main/java/bo/ucb/edu/ingsoft/dto/UserDto.java
@@ -0,0 +1,71 @@
+package bo.ucb.edu.ingsoft.dto;
+
+import bo.ucb.edu.ingsoft.model.Transaction;
+
+import java.util.Date;
+
+public class UserDto {
+    private Integer userId;
+    private String username;
+    private String password;
+    private String name;
+    private String lastName;
+    private String email;
+    private Integer privilege;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(Integer privilege) {
+        this.privilege = privilege;
+    }
+}
---
+++ src/main/java/bo/ucb/edu/ingsoft/model/User.java
@@ -0,0 +1,111 @@
+package bo.ucb.edu.ingsoft.model;
+
+import java.util.Date;
+
+public class User {
+    private Integer userId;
+    private String username;
+    private String password;
+    private String name;
+    private String lastName;
+    private String email;
+    private Integer privilege;
+    private Integer status;
+    private Transaction transaction;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(Integer privilege) {
+        this.privilege = privilege;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getTxId() {
+        return transaction.getTxId();
+    }
+
+    public void setTxId(Integer txId) {
+        this.transaction.setTxId(txId);
+    }
+
+    public String getTxHost() {
+        return transaction.getTxHost();
+    }
+
+    public void setTxHost(String txHost) {
+        this.transaction.setTxHost(txHost);
+    }
+
+    public Integer getTxUserId() {
+        return transaction.getTxUserId();
+    }
+
+    public void setTxUserId(Integer txUserId) {
+        this.transaction.setTxUserId(txUserId);
+    }
+
+    public Date getTxDate() {
+        return transaction.getTxDate();
+    }
+
+    public void setTxDate(Date txDate) {
+        this.transaction.setTxDate(txDate);
+    }
+}
