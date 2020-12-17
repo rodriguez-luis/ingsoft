ecd488c4dc8b9d307710be9a12fc77a92173d181
rodriguez-luis <pb.rndm@gmail.com>
2020-12-17 04:19:25 -0400

    create cart details dto

---
+++ src/main/java/bo/ucb/edu/ingsoft/dto/CartDetailsDto.java
@@ -0,0 +1,56 @@
+package bo.ucb.edu.ingsoft.dto;
+
+import bo.ucb.edu.ingsoft.model.Transaction;
+
+import java.util.Date;
+
+public class CartDetailsDto {
+    private Integer detailsId;
+    private Integer productId;
+    private Integer cartId;
+    private Integer quantity;
+    private Double price;
+
+    public CartDetailsDto() {
+    }
+
+    public Integer getDetailsId() {
+        return detailsId;
+    }
+
+    public void setDetailsId(Integer detailsId) {
+        this.detailsId = detailsId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Integer cartId) {
+        this.cartId = cartId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
