6f900dcc72a72f07409a7bc55302e336e13aa16c
rodriguez-luis <pb.rndm@gmail.com>
2020-12-16 23:01:05 -0400

    create product dto class

---
+++ src/main/java/bo/ucb/edu/ingsoft/dto/ProductDto.java
@@ -0,0 +1,98 @@
+package bo.ucb.edu.ingsoft.dto;
+
+import bo.ucb.edu.ingsoft.model.Transaction;
+
+import java.util.Date;
+
+public class ProductDto {
+    private Integer productId;
+    private String productName;
+    private String model;
+    private String productDescription;
+    private Integer stock;
+    private Double weight;
+    private Double unitPrice;
+    private Integer currency;
+    private Integer brand;
+
+    public java.lang.String getImg() {
+        return img;
+    }
+
+    public void setImg(java.lang.String img) {
+        this.img = img;
+    }
+
+    private String img;
+    public Integer getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Integer currency) {
+        this.currency = currency;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Integer brand) {
+        this.brand = brand;
+    }
+}
