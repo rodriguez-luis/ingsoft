02f7825ee133c448c1b7d3cc32baf37353e65a57
rodriguez-luis <pb.rndm@gmail.com>
2020-12-17 11:19:25 -0400

    create order model

---
+++ src/main/java/bo/ucb/edu/ingsoft/model/Order.java
@@ -0,0 +1,127 @@
+package bo.ucb.edu.ingsoft.model;
+
+import java.util.Date;
+
+public class Order {
+    private Integer orderId;
+    private Date date;
+    private Double total;
+    private String address;
+    private Integer cartId;
+    private String username;
+    private Integer deliveryBoy;
+    private Integer orderStatus;
+    private Integer status;
+    private Transaction transaction;
+
+    public Order(){
+        this.transaction= new Transaction();
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Integer cartId) {
+        this.cartId = cartId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getDeliveryBoy() {
+        return deliveryBoy;
+    }
+
+    public void setDeliveryBoy(Integer deliveryBoy) {
+        this.deliveryBoy = deliveryBoy;
+    }
+
+    public Integer getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(Integer orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public void setTransaction(Transaction transaction) {
+        this.transaction = transaction;
+    }
+    public Integer getTxId() {
+        return transaction.getTxId();
+    }
+
+    public void setTxId(Integer txId) {
+        this.transaction.setTxId(txId);
+    }
+
+    public String getTxHost() {
+        return transaction.getTxHost();
+    }
+
+    public void setTxHost(String txHost) {
+        this.transaction.setTxHost(txHost);
+    }
+
+    public Integer getTxUserId() {
+        return transaction.getTxUserId();
+    }
+
+    public void setTxUserId(Integer txUserId) {
+        this.transaction.setTxUserId(txUserId);
+    }
+
+    public Date getTxDate() {
+        return transaction.getTxDate();
+    }
+
+    public void setTxDate(Date txDate) {
+        this.transaction.setTxDate(txDate);
+    }
+}
