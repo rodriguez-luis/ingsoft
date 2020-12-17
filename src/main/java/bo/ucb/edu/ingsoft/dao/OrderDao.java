aa11396a75fab528750acaccb7a435c53e518713
rodriguez-luis <pb.rndm@gmail.com>
2020-12-17 11:34:30 -0400

    add order dao

---
+++ src/main/java/bo/ucb/edu/ingsoft/dao/OrderDao.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.ingsoft.dao;
+
+import bo.ucb.edu.ingsoft.model.Order;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface OrderDao {
+    public void create(Order order);
+    public List<Order> getOrders();
+    public void update(Order order);
+}
